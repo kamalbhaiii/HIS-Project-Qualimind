d6cd244dc05064b6fc130b464e1e2e1a2d3fb9ee
kamalbhaiii <kamal5201ks@gmail.com>
2025-11-14 20:19:24 +0100

    SCRUM-92: Fixed

--- server/src/loaders/prisma.ts
+++ server/src/loaders/prisma.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from '@prisma/client';
+import {PrismaClient} from '../../prisma/.prisma/client';
 import cfg from '@config/index';
 
 // Pass DB URL from JSON directly at runtime (no env file needed)
