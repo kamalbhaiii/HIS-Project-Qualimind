9915a37e67fddbff70c358c21c99a8f10d8f84b9
kamalbhaiii <kamal5201ks@gmail.com>
2026-01-04 12:01:14 +0100

    SCRUM-208: Changed dataset-view page route

--- frontend/src/pages/Dataset-upload/index.jsx
+++ frontend/src/pages/Dataset-upload/index.jsx
@@ -4,7 +4,7 @@ import ProtectedRoute from "../../routes/ProtectedRoute";
 
 export default function UploadDataset() {
   return <ProtectedRoute>
-    <DashboardLayout activeKey="datasets">
+    <DashboardLayout activeKey="dataset-upload">
     <UploadDatasetPageTemplate />
     </DashboardLayout>
   </ProtectedRoute>;
